ebfcad875d367a2eb8f714fc14cb9be9b84d57a5
SavirGordon <savirgordon83@gmail.com>
2022-12-12 11:49:00 -0500

    k

--- Lab - Protocols.playground/Pages/1. Exercise - Adopt Protocols.xcplaygroundpage/Contents.swift
+++ Lab - Protocols.playground/Pages/1. Exercise - Adopt Protocols.xcplaygroundpage/Contents.swift
@@ -3,25 +3,48 @@
  
  Create a `Human` class with two properties: `name` of type `String`, and `age` of type `Int`. You'll need to create a memberwise initializer for the class. Initialize two `Human` instances.
  */
-
+class Human: CustomStringConvertible {
+    let name: String
+    let age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    var description: String {
+        return "Human(name: \(name), age: \(age))"
+    }
+    
+    static func ==(lhs: Human, rhs: Human) -> Bool {
+        return lhs.name == rhs.name && lhs.age == rhs.age
+    }
+    
+    static func < (h1: Human, h2: Human) -> Bool {
+        return h1.age < h2.age
+    }
+}
+let h1 = Human(name: "Savir", age: 17)
+let h2 = Human(name: "John", age: 18)
 
 /*:
  Make the `Human` class adopt the `CustomStringConvertible`. Print both of your previously initialized `Human` objects.
  */
-
-
+print(h1)
+print(h2)
 /*:
  Make the `Human` class adopt the `Equatable` protocol. Two instances of `Human` should be considered equal if their names and ages are identical to one another. Print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are equal to eachother (using `==`). Then print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are not equal to eachother (using `!=`).
  */
-
+print(h1 == h2)
 
 /*:
  Make the `Human` class adopt the `Comparable` protocol. Sorting should be based on age. Create another three instances of a `Human`, then create an array called `people` of type `[Human]` with all of the `Human` objects that you have initialized. Create a new array called `sortedPeople` of type `[Human]` that is the `people` array sorted by age.
  */
-
-
-
 import Foundation
 
-
+var humans:[Human] = [h1, h2]
+var sortedHuman = humans.sorted(by: <)
+for h in sortedHuman{
+    print(h)
+}
 //: page 1 of 2  |  [Next: App Exercise - Printable Workouts](@next)
